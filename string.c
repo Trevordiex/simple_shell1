f6396741f01551191447b424480eb0bf8f3748f8
Dollarman36 <dollarmannnadi@gmail.com>
2022-11-16 14:18:01 -0800

    string.c

--- string.c
+++ string.c
@@ -5,10 +5,10 @@
 /**
  * _memcpy - copies a characters from src memory area to target mem area
  * @dest: destination mem area
- * @src: source mem area
- * @n: number of characters to copy
+ * @src: source mem area.
+ * @n: number of characters to copy.
  *
- * Return: returns thea address of dest
+ * Return: returns thea address of dest.
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
