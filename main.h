2b7ad583314f0eb1e39f06be807c92c734f1e197
Dollarman36 <dollarmannnadi@gmail.com>
2022-11-16 16:48:18 -0800

    main.h

--- main.h
+++ main.h
@@ -1,15 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <limits.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
  * struct path - a struct item for linked list of paths
@@ -58,26 +51,5 @@ int is_equal(char *s1, char *s2);
 char *malloc_str(char *);
 char *_memcpy(char *dest, char *src, unsigned int n);
 size_t _strlen(char *str);
-
-extern char **environ;
-
-char *show_input(void);
-void prompt(void);
-char *_strcat(char *src);
-int _strlen(char *str);
-void place(char *str);
-char *findfile(char *command);
-char *find_command(char *command);
-int compare(char *s1, char *s2);
-int _strcmpdir(char *s1, char *s2);
-int charput(char c);
-void place(char *str);
-char *str_concat(char *s1, char *s2);
-int lookforslash(char *cmd);
-int compareExit(char *s1, char *s2);
-int compareEnv(char *s1, char *s2);
-void execute_proc(char **cmd);
-char **identify_string(char *parameter);
-void controlC(int sig);
 
 #endif
