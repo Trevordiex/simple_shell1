e0070d5fdb7cf946056911f02e64c7fb08e6c48e
Dollarman36 <dollarmannnadi@gmail.com>
2022-11-16 16:34:01 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -1,46 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * main - A super simple shell
- * @argc: the argument count
- * @argv: argument vector - array of arguments
- * @env: the current environment
- *
- * Description: runs command just as in a shell
- *
- * Return: exit status
- */
-int main(int argc, char **argv, char **env)
+ * main - func with infinite loop
+ * @ac: No use
+ * @av: No use
+ * Return: loop.
+ **/
+
+int main(int ac, char **av)
 {
-	char *line = NULL;
-	size_t n = 0;
-	ssize_t len;
-	void (*cmd)(void);
+	(void)av;
+	(void)ac;
 
-	argc++;
-	do {
-		if (line)
-		{
-			if (line[len - 1] == '\n')
-				line[len - 1] = '\0';
-
-			cmd = get_builtin(line);
-			if (cmd)
-				(*cmd)();
-			else
-				run_command(argv[0], line, env);
-		}
-
-		if (isatty(STDIN_FILENO))
-			print("#cisfun$ ", "");
-		len = getline(&line, &n, stdin);
-	} while (len != -1);
-
-	free(line);
-	exit(EXIT_SUCCESS);
+	signal(SIGINT, controlC);
+	prompt();
+	return (0);
 }
-
